f4b7dfc471a5396e66f55c266424283653b7c358
Bhuwan Subedi <bsubedi@xylontech.com>
2026-04-29 07:27:31 +0545

    Task setup1: initial commit to setup the repo

---
+++ SkillBridge.Api/Repositories/JobRepository.cs
@@ -0,0 +1,13 @@
+public class JobRepository : IJobRepository
+{
+    public async Task<IEnumerable<JobDto>> GetJobListAsync()
+    {
+        return new List<JobDto>
+        {
+            new JobDto { Id = 1, Name = "Software Engineer",
+             Description = "Develop and maintain software applications" },
+            new JobDto { Id = 2, Name = "Product Manager",
+             Description = "Manage product development and strategy" }
+        };
+    }
+}
